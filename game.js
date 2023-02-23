33ab279daf49c5942219f8961f35f3e737c0fa5b
Husseinturfah <@husseinturfah>
2023-02-24 00:59:35 +0200

    added a listner on clicking the start button to color the borders black again, to prepare it for restarting the game later on

--- game.js
+++ game.js
@@ -1,19 +1,20 @@
 window.onload = function() {
   var boundary1 = document.querySelectorAll('div.boundary');
-  
+  var start = document.getElementById('start');
+
+
+
+
   for (var i = 0; i < boundary1.length; i++) {
     boundary1[i].addEventListener("mouseover", function() {
       for (var j = 0; j < boundary1.length; j++) {
         boundary1[j].style.borderColor = "red";}
       })
     }
+  start.addEventListener("click", function(){
+    console.log('clicked')
   for (var i=0; i< boundary1.length; i++) {
-    boundary1[i].addEventListener('mouseout', function() {
-
-      for (var j = 0; j < boundary1.length; j++) {
-        boundary1[j].style.borderColor = "black"; 
+        boundary1[i].style.borderColor = "black"; 
       }
     })
   }
-}
-
