30f8b76abd2ff4f05e6c168990097f7b0461bb06
Husseinturfah <@husseinturfah>
2023-02-24 01:13:13 +0200

    Added a listner that when clicking the end button says you won, and added several other statuses like when touching the walls or pressing the start button.

--- game.js
+++ game.js
@@ -1,20 +1,25 @@
 window.onload = function() {
   var boundary1 = document.querySelectorAll('div.boundary');
   var start = document.getElementById('start');
-
-
-
-
+  var end = document.getElementById('end')
   for (var i = 0; i < boundary1.length; i++) {
     boundary1[i].addEventListener("mouseover", function() {
       for (var j = 0; j < boundary1.length; j++) {
-        boundary1[j].style.borderColor = "red";}
-      })
-    }
-  start.addEventListener("click", function(){
-    console.log('clicked')
-  for (var i=0; i< boundary1.length; i++) {
-        boundary1[i].style.borderColor = "black"; 
+        boundary1[j].style.borderColor = "red";
+        document.getElementById('status').innerHTML = "You lost";
       }
     })
   }
+  start.addEventListener("click", function(){
+    console.log('clicked')
+    document.getElementById('status').innerHTML = 'Begin by moving your mouse over the "S".';
+  for (var i=0; i< boundary1.length; i++) {
+        boundary1[i].style.borderColor = "black"; 
+    }
+  })
+end.addEventListener("click", function(){
+  console.log('the end')
+  document.getElementById('status').innerHTML = "You won"
+} 
+)
+}
