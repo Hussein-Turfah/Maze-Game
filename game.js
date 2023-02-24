6e53764899a9d76fbc7cb3b432651089a30384d7
Husseinturfah <@husseinturfah>
2023-02-24 02:55:27 +0200

    Fixed some bugs, like hitting the end button and the walls several times.

--- game.js
+++ game.js
@@ -3,37 +3,45 @@ window.onload = function() {
   var start = document.getElementById('start');
   var end = document.getElementById('end');
   var score = document.querySelector('.example')
+  var y = 0;
   var x = 0;
-  score.innerHTML = x
+  score.innerHTML = x;
+
   //start or restart and reset
   start.addEventListener("click", function(){
     x = 0
+    y = 0
     score.innerHTML = x
     console.log('clicked')
     document.getElementById('status').innerHTML = 'Begin by moving your mouse over the "S".';
-  for (var i=0; i< boundary1.length; i++) {
+    for (var i=0; i< boundary1.length; i++) {
         boundary1[i].style.borderColor = "black"; 
     }
   })
   //lose
   for (var i = 0; i < boundary1.length; i++) {
-    boundary1[i].addEventListener("mouseover", function() {
+    boundary1[i].addEventListener("mouseover", function() { 
+      if(y==0){
       x -= 10
-    score.innerHTML = x
+      score.innerHTML = x
       for (var j = 0; j < boundary1.length; j++) {
         boundary1[j].style.borderColor = "red";
-        document.getElementById('status').innerHTML = "You lost";
+        document.getElementById('status').innerHTML = "You lost"; 
+        y+=5
+       }}
       }
-    })
+    )}
 
-  }
+
   //end which means win
-end.addEventListener("click", function(){
+  end.addEventListener("mouseover", function(){
   console.log('the end')
+  if (y == 0){
   document.getElementById('status').innerHTML = "You won";
   x+=5
   score.innerHTML = x;
   console.log('Score out')
-} 
+  y+=5
+}}
 )
 }
